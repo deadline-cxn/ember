eb2d4d96f34f60192c9afb8913a68e1c2e7299ef
sethcoder <defectiveseth@gmail.com>
2020-04-26 04:25:15 -0400

    getting ember working with new dlstorm

--- manserver.client.h
+++ manserver.client.h
@@ -3,58 +3,57 @@
 #ifndef _MANTRA_S_CLIENT_H
 #define _MANTRA_S_CLIENT_H
 
-#include "dlstorm.h"
-#include "network_messages.h"
+#include "c_gui_chat_enum.h"
 #include "c_log.h"
 #include "c_net.h"
+#include "c_serverinfo.h"
 #include "c_timer.h"
-#include "mantra_userdata.h"
+#include "dlstorm.h"
 #include "manserver.server.h"
 #include "mantra_common.h"
-#include "mantra_version.h"
-#include "c_serverinfo.h"
-#include "c_gui_chat_enum.h"
 #include "mantra_player_character.h"
+#include "mantra_userdata.h"
+#include "mantra_version.h"
+#include "net_messages.h"
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 class CServer;
 class CMPCharacter;
 
 class C_GSC : public CCSocket, public C_UserData {
-public:
-    C_GSC(CLog *pInLog,CServer *pInServer);
+   public:
+    C_GSC(CLog *pInLog, CServer *pInServer);
     ~C_GSC();
 
-    void    load(void);
-    void    save(void);
+    void load(void);
+    void save(void);
 
-    CServer *pServer;   // parent server object
-    void    db_update(string s_col,string s_val);
+    CServer *pServer;  // parent server object
+    void     db_update(string s_col, string s_val);
 
-    CLog    *pLog;      // log object
-	bool    bMadeLog;   // internal destruction flag
+    CLog *pLog;      // log object
+    bool  bMadeLog;  // internal destruction flag
 
-    bool    bDelete;        // flag for object destruction
-    bool    bHeartBeatCheck;// heartbeat flag
+    bool bDelete;          // flag for object destruction
+    bool bHeartBeatCheck;  // heartbeat flag
 
-    CTimer  *inactivity_timer;
-    CTimer  *heartbeat_timer;
+    CTimer *inactivity_timer;
+    CTimer *heartbeat_timer;
 
-    C_GSC   *pNext;
-    C_GSC   *pPrevious;
+    C_GSC *pNext;
+    C_GSC *pPrevious;
 
     CMPCharacter *character;
 
-    void    initc(void);
+    void initc(void);
 
-    void    do_script(char *file);
-    void    do_func(char *func);
-    void    do_net(void);
+    void do_script(char *file);
+    void do_func(char *func);
+    void do_net(void);
 
     // commands to push net messages to the actual client follow :
-    bool    gui_clear(void);
-    bool    gui_call(char *name);
-
+    bool gui_clear(void);
+    bool gui_call(char *name);
 };
 
-#endif // _MANTRA_S_CLIENT_H
+#endif  // _MANTRA_S_CLIENT_H
